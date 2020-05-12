80bdd4a2a62250e1c8cb57cf4f199af154f504b2
nienkenoort <64905887+nienkenoort@users.noreply.github.com>
2020-05-12 15:43:53 +0200

    Update output.py

--- output.py
+++ output.py
@@ -1,2 +1,2 @@
-'''init def is voor parameters aan een class geven? dus bij class Output, en def __init__(self, sentence, add), roep je de class aan met Ouput(zin, woord).'''
 class Output:
+    def __init__(self):
