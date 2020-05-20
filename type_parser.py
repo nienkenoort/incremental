2b0766d531ccac0802a409c30b9a08ea080574a1
nienkenoort <64905887+nienkenoort@users.noreply.github.com>
2020-05-20 10:12:10 +0200

    Update type_parser.py

--- type_parser.py
+++ type_parser.py
@@ -21,29 +21,3 @@ def unfoldExp(expL):
 example = "N \\ (S / NP)"
 result = strToPol(example) # dit geeft '\ N / S NP'
 print(result)
-
-'''
-import string
-from pyparsing import Word, Literal, opAssoc, infixNotation
-
-atom_ = Word(string.ascii_uppercase)
-#typedec_ = Word(string.ascii_lowercase + '_')
-under = Literal("\\")
-over = Literal("/")
-arrowexp_ = atom_ + over
-
-typeexp_ = infixNotation(atom_, [(arrowexp_, 1, opAssoc.RIGHT)],)
-
-def strToPol(str_):
-    return unfoldExp(typeexp_.parseString(str_).asList()[0])
-
-def unfoldExp(expL):
-    if isinstance(expL, str):
-        return expL
-    else:
-        return ' '.join([unfoldExp(expL[1]), unfoldExp(expL[0]), unfoldExp(expL[2])])
-
-example = "NP / KP / S"
-result = strToPol(example) # dit geeft 'obj NP subj NP S'
-print(result)
-'''
