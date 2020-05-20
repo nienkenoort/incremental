736e4a02121ee65e95428ad54e32dcb3b603e005
nienkenoort <64905887+nienkenoort@users.noreply.github.com>
2020-05-20 10:20:42 +0200

    Update type_parser.py

--- type_parser.py
+++ type_parser.py
@@ -5,9 +5,10 @@ atom_ = Word(string.ascii_uppercase)
 #typedec_ = Word('(' + '_')
 under = Literal("\\")
 over = Literal("/")
+tensor = Literal("*")
 #arrowexp_ = over + typedec_ 
 
-typeexp_ = infixNotation(atom_, [(over, 2, opAssoc.RIGHT), (under, 2, opAssoc.RIGHT), ('(', 2, opAssoc.RIGHT), (')', 2, opAssoc.LEFT)],)
+typeexp_ = infixNotation(atom_, [(over, 2, opAssoc.RIGHT), (under, 2, opAssoc.RIGHT), (tensor, 2, opAssoc.RIGHT), ('(', 2, opAssoc.RIGHT), (')', 2, opAssoc.LEFT)],)
 
 def strToPol(str_):
     return unfoldExp(typeexp_.parseString(str_).asList()[0])
@@ -18,6 +19,13 @@ def unfoldExp(expL):
     else:
         return ' '.join([unfoldExp(expL[1]), unfoldExp(expL[0]), unfoldExp(expL[2])])
 
-example = "N \\ (S / NP)"
+example = "(N \\ S) / NP"
 result = strToPol(example) # dit geeft '\ N / S NP'
 print(result)
+
+typeexp_.runTests('''
+       (N \\ S) / NP
+       N / (S \\ NP)
+       N * N
+       (N * N) \\ NP
+       ''', fullDump=True)
