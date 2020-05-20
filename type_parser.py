e34ff13fa27e3304a15ef11e7de6f6d5ee538427
nienkenoort <64905887+nienkenoort@users.noreply.github.com>
2020-05-20 10:45:56 +0200

    Update type_parser.py

--- type_parser.py
+++ type_parser.py
@@ -1,31 +1,13 @@
 import string
 from pyparsing import Word, Literal, opAssoc, infixNotation
 
-atom_ = Word(string.ascii_uppercase)
-#typedec_ = Word('(' + '_')
+typestring = Word(string.ascii_uppercase)
 under = Literal("\\")
 over = Literal("/")
 tensor = Literal("*")
-#arrowexp_ = over + typedec_ 
 
-typeexp_ = infixNotation(atom_, [(over, 2, opAssoc.RIGHT), (under, 2, opAssoc.RIGHT), (tensor, 2, opAssoc.RIGHT)],)
+typeexpr = infixNotation(typestring, [(over, 2, opAssoc.RIGHT), (under, 2, opAssoc.RIGHT), (tensor, 2, opAssoc.RIGHT)],)
 
-def strToPol(str_):
-    return unfoldExp(typeexp_.parseString(str_).asList()[0])
-
-def unfoldExp(expL):
-    if isinstance(expL, str):
-        return expL
-    else:
-        return ' '.join([unfoldExp(expL[1]), unfoldExp(expL[0]), unfoldExp(expL[2])])
-
-example = "(N \\ S) / NP"
-result = strToPol(example) # dit geeft '\ N / S NP'
-print(result)
-
-typeexp_.runTests('''
-       (N \\ S) / NP
-       N / (S \\ NP)
-       N * N
-       (N * N) \\ NP
-       ''', fullDump=True)
+inputtype = "(N \\ S) / NP"
+typelist = typeexpr.parseString(inputtype).asList()[0]
+print(typelist)
