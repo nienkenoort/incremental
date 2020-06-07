5111d73100c436838b9ff055ed1648657f88a59d
nienkenoort <64905887+nienkenoort@users.noreply.github.com>
2020-06-07 18:52:48 +0200

    Update lexicon_parser.py

--- lexicon_parser.py
+++ lexicon_parser.py
@@ -1,8 +1,37 @@
 class Lexicon:
+    """
+    A class that forms a dictionary of all of the words with their corresponding types that are necessary for building a proofnet of any random sentence.
+    
+    ...
+
+    Attributes
+    ----------
+    lexicon : dict()
+        name of the dictionary in which the words and their types are saved.
+
+    Methods
+    -------
+    createLexicon()
+        Adds a type to each word that is in the dictionary.
+    """
     def __init__(self):
+        """
+        Attributes
+        ----------
+        lexicon : dict()
+            name of the dictionary in which the words and their types are saved.
+        """
         self.lexicon = dict()
        
     def createLexicon(self):
+        """
+        Gets an empty dictionary and adds words with their corresponding type to the dictionary.
+
+        Returns
+        -------
+        dictionary
+            a dictionary of all words with their types of which we can choose when creating a proofnet.
+        """
         lexicon = self.lexicon
         #wanneer ik de lexicon ga importeren kan ik hier wel een loop van maken die de elementen automatisch toevoegt zonder dat ik alles los moet doen
         lexicon["de"] = "NP/N"
@@ -15,8 +44,7 @@ class Lexicon:
 def main():
     obj = Lexicon()
     obj.createLexicon()
-    #for key, value in obj.lexicon.items():
-        #print(key, value)
+
 if __name__ == '__main__':
     main()
         
