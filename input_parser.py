4d28470fdebe6c6b2e9384693fbad4fa50b61c57
nienkenoort <64905887+nienkenoort@users.noreply.github.com>
2020-06-19 13:04:31 +0200

    Update input_parser.py

--- input_parser.py
+++ input_parser.py
@@ -8,7 +8,8 @@ import linkedlist
 class Input:
     """
     A class that gets a string as parameter and creates a Linked List of that string. The Linked List will include 
-    each word of the string with their corresponding type and polarity.
+    each word of the string with their corresponding type and polarity. It will do so by using the other parameter: a string 
+    that saves the resulting type of the sentence.
     
     ...
 
@@ -16,6 +17,8 @@ class Input:
     ----------
     sentence : str
         name of the string that was read from the console and will be become the Linked List.
+    sentence : str
+        name of the string that was read from the console and will be become the type of the sentence.
 
     Methods
     -------
@@ -23,14 +26,17 @@ class Input:
         Creates a Linked List of the input string by creating a Linked List object using the linkedlist.py file 
         and filling this list with the words from the input string and their corresponding types and polarity values.
     """
-    def __init__(self, sentence):
+    def __init__(self, sentence, resultingType):
         """
         Parameters
         ----------
         sentence : str
             The name of the string that is the input from the user
+        resultingType : str
+            The type of the full sentence.
         """
         self.sentence = sentence
+        self.resultingType = resultingType
 
     def parser(self):
         """
@@ -42,8 +48,6 @@ class Input:
             a list of all words from the input sentence including their type and polarity value.
         """
         linkedList = linkedlist.LinkedList() #Creates an empty Linked List
-        linkedList.add((None, "NP", 0)) #Adds the resulting type to the list
-        #Let 0 be an output polarity and 1 be an input polarity
         sentence_list = self.sentence.split()
         sentence_list.reverse()
         #Check the corresponding type for each word that was in the sentence by using the Lexicon class
@@ -52,18 +56,25 @@ class Input:
         for sentence_word in sentence_list:
             for lexicon_word, lexicon_type in lexicon_obj.lexicon.items():
                 if sentence_word == lexicon_word:
+                    #Let 0 be an output polarity and 1 be an input polarity
                     linkedList.add((sentence_word, lexicon_type, 1))
                 else:
-                    #print(sentence_word + 'does not appear as real word')
-                    None #moet dit nog even aanpassen want anders pakt hij ook niet bestaande woorden?
+                    None
         node = linkedList.root
+        linkedList.add((None, self.resultingType, 0)) #Adds the resulting type to the list
         return linkedList
 
 class Read:
     """
-    A class that reads the input string from the console and uses the "Input" class to create a Linked List of that string.
+    A class that reads the input string from the console and uses the "Input" class to create a Linked List of that string. 
+    It also determines if the user wants to follow the process of the incrementally build sentence and it saves the type of the sentence.
     
     """
     sentence = input('Enter your sentence: \n') #The user enters his/her sentence
-    obj = Input(sentence)
+    resultingType = input('What needs to be the resulting type of the sentence? Make sure that the type is written in capslock. \n') #The user enters the type of the sentence
+    obj = Input(sentence, resultingType)
     lijst = obj.parser()
+    incrementalSentence = input('Do you want to be able to follow the process of the incrementally build sentence? (y/n): \n') #The user enters whether he wants to follow the process.
+    incremental = False
+    if(incrementalSentence == "y" or incrementalSentence == "yes" or incrementalSentence == "Y" or incrementalSentence == "Yes"):
+        incremental = True
